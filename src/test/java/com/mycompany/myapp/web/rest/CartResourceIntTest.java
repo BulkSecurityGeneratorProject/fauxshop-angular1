1364717d9a7245ee695c2ea2df89f51eaaf785d1
derekzuk <derekzuk@gmail.com>
2017-10-18 17:32:39 -0400

    Added testAddToCartWithExistingItemInCart() unit test

--- src/test/java/com/mycompany/myapp/web/rest/CartResourceIntTest.java
+++ src/test/java/com/mycompany/myapp/web/rest/CartResourceIntTest.java
@@ -175,4 +175,46 @@ public class CartResourceIntTest {
             .andExpect(jsonPath("$.cartItemQuantity").value("5"))
             .andExpect(jsonPath("$.cartItemTotalPrice").value("10"));
     }
+
+    @Test
+    @Transactional
+    public void testAddToCartWithExistingItemInCart() throws Exception {
+        List<Cart> cartList = new ArrayList<>();
+        Cart cart = new Cart();
+        cart.setCartId(1L);
+        cart.setId(2L);
+        cart.setProductsId(3L);
+        cart.setCartItemQuantity(5);
+        cart.setCartItemTotalPrice(BigDecimal.TEN);
+        cartList.add(cart);
+        Optional<List<Cart>> cartOptional = Optional.of(cartList);
+
+        Cart updatedCart = new Cart();
+        updatedCart.setCartId(1L);
+        updatedCart.setId(2L);
+        updatedCart.setProductsId(3L);
+        updatedCart.setCartItemQuantity(10);
+        updatedCart.setCartItemTotalPrice(BigDecimal.valueOf(20));
+
+        Products products = new Products();
+        products.setProductsId(3L);
+        products.setProductsQuantity(55);
+        products.setProductsPrice(BigDecimal.TEN);
+        Optional<Products> productsOptional = Optional.of(products);
+
+        when(mockProductsService.getProductsByProductsId(3L)).thenReturn(productsOptional);
+        when(mockCartService.findAllById(2L)).thenReturn(cartOptional);
+        when(mockCartService.save(updatedCart)).thenReturn(updatedCart);
+
+        restUserMockMvc.perform(post("/api/cart/2/3/5")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.cartId").value("1"))
+            .andExpect(jsonPath("$.id").value("2"))
+            .andExpect(jsonPath("$.productsId").value("3"))
+            .andExpect(jsonPath("$.cartItemQuantity").value("10"))
+            .andExpect(jsonPath("$.cartItemTotalPrice").value("20"));
+    }
 }
